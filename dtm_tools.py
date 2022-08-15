0a34b7efd8c15ebc7776e52ea08983f3be119dad
Joshua Teves <joshua.teves@nih.gov>
2022-08-15 16:30:32 -0400

    Updates dtm_tools

--- dtm_tools.py
+++ dtm_tools.py
@@ -2,7 +2,25 @@
 
 from argparse import ArgumentParser
 import pandas as pd
+import numpy as np
 
+TAG = 'classification_tags'
+KUNDU_TAGS = ('Accept borderline', 'No provisional accept')
+RATIONALE_TABLE = {
+    'I001': 'Manual classification',
+    'I002': 'Rho > Kappa',
+    'I003': 'More significant voxels S0 vs. R2',
+    'I004': 'S0 Dice > R2 Dice AND high varex',
+    'I005': 'Noise F-value > Signal F-value AND high varex',
+    'I006': 'No good components found',
+    'I007': 'Mid-Kappa',
+    'I008': 'Low variance',
+    'I009': 'Mid-Kappa type A',
+    'I010': 'Mid-Kappa type B',
+    'I011': 'ign_add0',
+    'I012': 'ign_add1',
+    'N/A':  'N/A',
+}
 
 def get_table_type(table: pd.DataFrame) -> str:
     """Get the table type from the supplied table.
@@ -14,13 +32,16 @@ def get_table_type(table: pd.DataFrame) -> str:
     - kundu-dtm
     - minimal-dtm
     """
-    if "tags" not in table.columns:
+    if "classification_tags" not in table.columns:
         return "kundu-main"
     else:
-        has_low_variance = np.any(
-            ["Low variance" in tag_list for tag_list in table["tags"]]
-        )
-        if has_low_variance:
+        has_kundu_tag = False
+        for tag_list in table[TAG]:
+            for t in KUNDU_TAGS:
+                if t in tag_list:
+                    has_kundu_tag = True
+
+        if has_kundu_tag:
             return "kundu-dtm"
         else:
             return "minimal-dtm"
@@ -40,10 +61,10 @@ def get_classification(row: pd.Series) -> str:
         return "I"
     elif row["classification"] == "accepted":
         # tags indicate ignored for kundu DTM
-        if "tags" in row and (
-            "Low variance" in row["tags"] or
-            "Accept borderline" in row["tags"] or 
-            "No provisional accept" in row["tags"]
+        if TAG in row and (
+            "Low variance" in row[TAG] or
+            "Accept borderline" in row[TAG] or 
+            "No provisional accept" in row[TAG]
         ):
             return "I"
         else:
@@ -67,8 +88,8 @@ def main():
     lfile = args.left
     rfile = args.right
 
-    ltable = pd.read_csv(lfile)
-    rtable = pd.read_csv(rfile)
+    ltable = pd.read_csv(lfile, delimiter='\t')
+    rtable = pd.read_csv(rfile, delimiter='\t')
 
     assert "classification" in ltable.columns
     assert "classification" in rtable.columns
@@ -110,19 +131,45 @@ def main():
 
     if len(change_summary.keys()) == 0:
         print("No differences in classification")
-    elif args.verbose:
-        print(f"CHANGE\tNC\tVAREX\tCOMPS")
     else:
-        print(f"CHANGE\tNC\tVAREX")
+        print(f"Change\tNumComponents\tVarex\tComponentIndices")
 
     for k, v in change_summary.items():
         n = len(v['components'])
         vx = v['varex']
-        summary = f"{k}\t{n:03}\t{vx:2.4f}"
-        if args.verbose:
-            allcomps = v['components']
-            summary += f"\t{allcomps}"
+        allcomps = v['components']
+        summary = f"{k}\t{n:03}\t\t{vx:2.4f}\t{allcomps}"
         print(summary)
+
+    if args.verbose:
+        # Print information for each component
+        comps = []
+        for _, v in change_summary.items():
+            for c in v['components']:
+                comps.append(c)
+        comps.sort()
+        if 'dtm' in ltype:
+            lcol = TAG
+        else:
+            lcol = 'rationale'
+        if 'dtm' in rtype:
+            rcol = TAG
+        else:
+            rcol = 'rationale'
+        rtkeys = RATIONALE_TABLE.keys()
+
+        for c in comps:
+            lcomp_val = ltable.iloc[c][lcol]
+            rcomp_val = rtable.iloc[c][rcol]
+            if lcomp_val in rtkeys:
+                lcomp_val = RATIONALE_TABLE[lcomp_val]
+            if rcomp_val in rtkeys:
+                rcomp_val = RATIONALE_TABLE[rcomp_val]
+            if str(lcomp_val) == "nan":
+                lcomp_val = 'N/A'
+            if str(rcomp_val) == "nan":
+                rcomp_val = 'N/A'
+            print(f"{c}:\t{lcomp_val}\t{rcomp_val}")
 
 
 
